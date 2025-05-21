a73aef46f27e89c36c2e4e5dde996b75d85b7e00
svennikue <s.kuechenhoff@gmx.de>
2025-05-21 14:32:51 +0100

    one more typo

--- scripts/human_cells_perms_plot_elanet.py
+++ scripts/human_cells_perms_plot_elanet.py
@@ -148,7 +148,7 @@ if __name__ == "__main__":
     # For debugging, bypass Fire and call compute_one_subject directly.
     plot_all(
         model_name_string='w_partial_musicboxes_excl_rep1-1_avg_in_20_bins_across_runs',
-        sub_list=[1,2,3,4,5,6,8,9,10,11],
+        sub_list=[1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,18,19,20,21,22,23,24,25,26,28,29,30,31,32,33,34,35,36,38,39,40,41,42,43,44,45,46,48,49,50,51,52,53,54,55,56,58,59],
         perms_locs = '265perms_configs_shuffle',
         perms_time = '265perms_timepoints_shuffle',
         plot_cells_corr_higher_than=0.05,
